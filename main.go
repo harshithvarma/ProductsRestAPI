9c5ab4935f6141945fc3a67fad7e9a6b644501b4
harshithvarma <harshithvarma99@gmail.com>
2020-05-09 21:49:34 +0530

    basic routes and server setup

--- main.go
+++ main.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
+	"github.com/gorilla/mux"
+	"time"
 )
 
 func main(){
 	l:=log.New(os.Stdout,"product-api",log.LstdFlags)
+	s:=mux.NewRouter()
+	getRouter:=s.Methods(http.MethodGet).Subrouter()
+	putRouter:=s.Methods(http.MethodPut).Subrouter()
+	postRouter:=s.Methods(http.MethodPost).Subrouter()
+	delRouter:=s.Methods(http.MethodDelete).Subrouter()
+
+	server:=http.Server{
+		Addr:              ":9090",
+		Handler:           s,
+		TLSConfig:         nil,
+		ReadTimeout:       5*time.Second,
+		WriteTimeout:      10*time.Second,
+		IdleTimeout:       120*time.Second,
+	}
+
 }
