95589669962358d47510c273b57b55e66976324a
harshithvarma <harshithvarma99@gmail.com>
2020-05-10 12:47:41 +0530

    handlers updated with crud

---
+++ handlers/handlers.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"ProductRestAPI/data"
+	"context"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type ProductHandler struct {
+	l *log.Logger
+}
+
+func NewProductHandler(l *log.Logger) *ProductHandler {
+	return &ProductHandler{l}
+}
+
+func (p *ProductHandler) GetProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET products")
+	list := data.GetProductsList()
+	err := list.ToJson(rw)
+	if err != nil {
+		http.Error(rw, "unable to unmarshal to json", http.StatusInternalServerError)
+	}
+}
+
+type KeyProduct struct{}
+
+func (p *ProductHandler) AddProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST product")
+
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+	data.AddProduct(&prod)
+}
+
+func (p *ProductHandler) UpdateProduct(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(rw, "unable to convert id", http.StatusBadRequest)
+	}
+	p.l.Println("handle PUT product")
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+	err = data.UpdateProduct(id, &prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "product not found", http.StatusNotFound)
+	}
+}
+
+func (p *ProductHandler) MiddlewareValidateProduct(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := data.Product{}
+		err := prod.FromJson(r.Body)
+		if err != nil {
+			p.l.Println("[ERROR] deserializing product", err)
+			http.Error(rw, "Error reading product", http.StatusBadRequest)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(rw, r)
+
+	})
+}
