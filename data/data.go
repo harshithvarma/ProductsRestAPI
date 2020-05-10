2fd52ee04ae6327f072d9797235114a5df10debd
harshithvarma <harshithvarma99@gmail.com>
2020-05-10 11:05:23 +0530

    crud functions updated

--- data/data.go
+++ data/data.go
@@ -1,5 +1,11 @@
 package data
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
 type Product struct {
 	ID          int
 	Name        string
@@ -7,7 +13,51 @@ type Product struct {
 	price       int
 }
 
-var productsList=[]*Product{
+type ProductsList []*Product
+
+func (p *Product) ToJson(w io.Writer) error{
+	e:=json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+func (p *Product) FromJson(r io.Reader) error{
+	e:=json.NewDecoder(r)
+	return e.Decode(p)
+}
+func GetProductsList() ProductsList {
+	return productsList
+}
+func AddProduct(p *Product) {
+	id := GetId()
+	p.ID = id
+	productsList = append(productsList, p)
+}
+func UpdateProduct(id int, p *Product) error {
+	_,pos,err:=findProduct(id)
+	if err!=nil{
+		return err
+	}
+	p.ID=id
+	productsList[pos]=p
+	return nil
+}
+
+func GetId() int {
+	n := productsList[len(productsList)-1]
+	return n.ID + 1
+}
+
+var ErrProductNotFound = fmt.Errorf("product not found")
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productsList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil,-1,ErrProductNotFound
+}
+
+var productsList = []*Product{
 	&Product{
 		ID:          1,
 		Name:        "tea",
